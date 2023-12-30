9cfbf8bd761e7b0b688d0c51ad10a2377049031d
mowgly11 <oussamabouzalim55@gmail.com>
2023-12-30 17:36:26 +0100

    ability to send attachments

--- index.js
+++ index.js
@@ -1,58 +1,82 @@
 const nodemailer = require("nodemailer");
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+const wait = require("node:timers/promises").setTimeout;
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
-const fs = require('node:fs');
-const config = require('./config.json');
+const fs = require("node:fs");
+const config = require("./config.json");
 
 function input(prompt) {
-    return new Promise((callbackFunction, errorFunction) => {
-        readline.question(prompt, (userInput) => {
-            callbackFunction(userInput);
-        }, () => {
-            errorFunction();
-        });
-    });
+  return new Promise((callbackFunction, errorFunction) => {
+    readline.question(
+      prompt,
+      (userInput) => {
+        callbackFunction(userInput);
+      },
+      () => {
+        errorFunction();
+      }
+    );
+  });
 }
 
 const transporter = nodemailer.createTransport({
-    service: "hotmail",
-    auth: {
-        user: config.user,
-        pass: config.password
-    }
+  service: "hotmail",
+  auth: {
+    user: config.user,
+    pass: config.password,
+  },
 });
 
 async function getInfo() {
-    let subject = await input("Please specify a title/subject: ");
-    let text = await input("Please specify a text: ");
+  let subject = await input("Please specify a title/subject: ");
+  let attachment = await input("Please specify a file path you want to send: ");
 
-    const emailsFile = fs.readFileSync('./files/emails.txt', 'utf-8');
-    if(emailsFile === '') {
-        readline.close();
-        return console.log("Please don't forget to fill the emails file with emails.");
-    }
+  const emailsFile = fs.readFileSync("./files/emails.txt", "utf-8");
+  if (emailsFile === "") {
+    readline.close();
+    return console.log(
+      "Please don't forget to fill the emails file with emails."
+    );
+  }
 
-    if (subject == 0 || text == 0) {
-        readline.close();
-        return console.log("You can't send an empty email..");
-    }
+  if (subject == 0) {
+    readline.close();
+    return console.log("You can't send an empty email..");
+  }
 
-    let args = {
-        from: config.user,
-        to: emailsFile.split("\n"),
-        subject: subject,
-        text: text,
-        html: fs.readFileSync('./files/index.html', 'utf-8') ?? null
-    }
+  const html = fs.readFileSync("./files/index.html", "utf-8");
+  const text = fs.readFileSync("./files/text.txt", "utf-8");
+
+  const emails = emailsFile.split("\n");
+
+  let args = {
+    from: config.user,
+    subject,
+    text,
+    html: html.trim() === "" ? null : html,
+  };
+
+  for(let i = 0; i < emails.length; i++) {
+    await wait(5000);
+
+    args.to = emails[i];
+
+    if (attachment)
+      args.attachments = [
+        {
+          path: attachment,
+        },
+      ];
 
     transporter.sendMail(args, (err, info) => {
-        if (err) throw new Error(err);
-        console.log("Messages sent!");
+      if (err) throw new Error(err);
+      console.log("Messag sent to " + emails[i]);
     });
+  }
 
-    readline.close();
-};
+  readline.close();
+}
 
 getInfo();
